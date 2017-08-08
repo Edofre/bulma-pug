cda43193da5d83a9b96296b9933dab620bf09c1e
Edo Freriks <edofre@gmail.com>
2017-08-08 21:10:16 +0200

    Refactor navitems

--- helpers/functions.js
+++ helpers/functions.js
@@ -2,6 +2,6 @@
  * Capitalize the first letter of a string
  * @returns {string}
  */
-String.prototype.capitalizeFirstLetter = function() {
+String.prototype.ucFirst = function () {
     return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 }
